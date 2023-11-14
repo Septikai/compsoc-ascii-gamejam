a6d0f1184a7d05fa4861001e4305c5f0421872f4
Elliot Tollemache <114115893+ElLovesSWS@users.noreply.github.com>
2023-11-14 19:34:51 +0000

    added basic plans for RedRidingHood

---
+++ compsoc-ascii-gamejam/Stories/RedRidingHood.cs
@@ -0,0 +1,39 @@
+namespace compsoc_ascii_gamejam.Stories;
+
+public class redRidingHood //Starting story you launch into
+{
+    private static readonly redRidingHood Instance = new redRidingHood();
+
+    private redRidingHood()
+    {
+        /*introductory part of mom sending you on journey
+        
+        find some flowers along the way, collect
+        see some interesting looking rocks, collect
+        statement giving you option on what you want to do 
+
+        RUN:
+        agility roll - are you agile enough to escape??? 
+            Success:
+            Run away and come across some sheep. TRIGGER BOY WHO CRIED WOLF SEQUENCE
+
+            Fail:
+            Start running wolf catches you and hurts you (-3 hp) then you escape and keep running. TRIGGER BOY WHO CRIED WOLF SEQUENCE
+        
+        TALK:
+        charisma roll - can you sweet talk your way to get the wolf to be honest???
+            Success: 
+            Wolf tells you it's plans apologizes, and runs off never to be seen again dun dun duhhh (+1 WP)
+            Finish red riding hood sequence. Find snow white
+            
+            Fail:
+            Wolf gets upset with you, and attacks you (-4 hp) then runs off, and you decide to continue to grandma's. Only to find her DEAD and quickly flee before wolf can kill you.
+            Lose red riding hood. Get lose when leaving TRIGGER SNOW WHITE SEQUENCE
+
+        FIGHT:
+        strength roll 
+
+        */
+    }
+    
+}
