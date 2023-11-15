cd59b52d6c238f689e57733e345af90a88228a5f
JanHa <jan.halaczek@live.com>
2023-11-15 04:15:10 +0000

    initial version of storyManager finished, with a testStory.txt included

--- compsoc-ascii-gamejam/Story/StoryManager.cs
+++ compsoc-ascii-gamejam/Story/StoryManager.cs
@@ -1,16 +1,25 @@
-﻿namespace compsoc_ascii_gamejam.Stories;
+﻿using System.Text;
 
-class BiLinkedStoryNode
+namespace compsoc_ascii_gamejam.Stories;
+
+public class BiLinkedStoryNode
 {
-    private readonly BiLinkedStoryNode? nextNode;
-    private readonly List<Tuple<string, BiLinkedStoryNode>>? decisions;
-    private readonly string dialogue;
+    private BiLinkedStoryNode? nextNode;
+    private List<Tuple<string, BiLinkedStoryNode>>? decisions;
+    private string dialogue;
 
     public BiLinkedStoryNode(BiLinkedStoryNode? nextNode, List<Tuple<string, BiLinkedStoryNode>>? decisions, string dialogue)
     {
         this.nextNode = nextNode;
         this.decisions = decisions;
         this.dialogue = dialogue;
+    }
+    
+    public BiLinkedStoryNode()
+    {
+        this.nextNode = null;
+        this.decisions = null;
+        this.dialogue = "";
     }
 
     public string getDialogue()
@@ -50,13 +59,112 @@ class BiLinkedStoryNode
                     return value.Item2;
                 }
             }
-
+        Console.WriteLine("No such decision available");
         return this;
+    }
+
+    public void setNextNode(BiLinkedStoryNode newNode)
+    {
+        nextNode = newNode;
+    }
+    
+    public void setDialogue(string newDialogue)
+    {
+        dialogue = newDialogue;
+    }
+
+    public void addDecision(Tuple<string, BiLinkedStoryNode> newDecision)
+    {
+        decisions ??= new List<Tuple<string, BiLinkedStoryNode>>();
+        decisions.Add(newDecision);
     }
 
 }
 
 public class StoryManager
 {
-    private BiLinkedStoryNode root = new BiLinkedStoryNode(null, null, "Hello World");
+    private BiLinkedStoryNode root;
+
+
+    public StoryManager(string filepath)
+    {
+        root = loadFile(filepath);
+    }
+
+    public BiLinkedStoryNode loadFile(string filepath)
+    {
+        filepath = Path.Combine(Directory.GetCurrentDirectory(), filepath);
+        BiLinkedStoryNode? newRoot = new BiLinkedStoryNode();
+        List<string?> fileLines = new List<string?>();
+        using (var fileStream = File.OpenRead(filepath))
+        {
+            using (var streamReader = new StreamReader(fileStream, Encoding.UTF8, true)) {
+                string? line;
+                while ((line = streamReader.ReadLine()) != null)
+                {
+                    fileLines.Add(line);
+                }
+            }
+        }
+
+        Dictionary<int, BiLinkedStoryNode> createdNodes = new Dictionary<int, BiLinkedStoryNode>();
+        newRoot = RecursiveFileReader(ref fileLines, ref createdNodes, 0);
+
+        return newRoot;
+    }
+
+    private static BiLinkedStoryNode? RecursiveFileReader(ref List<string?> fileLines, ref Dictionary<int, BiLinkedStoryNode> createdNodes, int workingIndex)
+    {
+        
+        BiLinkedStoryNode currentWorkingNode = new BiLinkedStoryNode();
+
+        if (createdNodes.TryGetValue(workingIndex, out var knownNode))
+        {
+            return knownNode;
+        }
+        
+        var workingLine = fileLines[workingIndex];
+        var data = workingLine.Split(';');
+        currentWorkingNode.setDialogue(data[1]);
+        createdNodes.Add(workingIndex, currentWorkingNode);
+        if (data[2].Contains('{'))
+        {
+            data[2] = data[2].Replace("{", "");
+            data[2] = data[2].Replace("}", "");
+            var choices = data[2].Split("@");
+            for (var i = 0; i < choices.Length; i++)
+            {
+                var choice = choices[i].Split("_");
+                var dialogue = choice[0];
+                var nextLine = int.Parse(choice[1]);
+                //var item_given = int.Parse(choice[2]); //TODO: add items to the choice
+
+                if (workingIndex != nextLine)
+                {
+                    var newNode = RecursiveFileReader(ref fileLines, ref createdNodes, nextLine);
+                    if (newNode != null)
+                    {
+                        currentWorkingNode.addDecision(new Tuple<string, BiLinkedStoryNode>(dialogue, newNode));
+                    }
+                }
+                else
+                {
+                    currentWorkingNode.addDecision(new Tuple<string, BiLinkedStoryNode>(dialogue, currentWorkingNode));
+                }
+            }
+        }
+        else
+        {
+            
+            var newNode = RecursiveFileReader(ref fileLines, ref createdNodes, int.Parse(data[2]));
+            if (newNode != null)
+            {
+                currentWorkingNode.setNextNode(newNode);
+            }
+        }
+
+        createdNodes[workingIndex] = currentWorkingNode;
+        
+        return currentWorkingNode;
+    }
 }
