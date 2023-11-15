16d273af5046e4cbc4970da8ed5b02958a3d524b
Natalia Spence <52349324+Septikai@users.noreply.github.com>
2023-11-15 00:26:46 +0000

    make CharacterStat.MaxHealth.ToString() be more user friendly

--- compsoc-ascii-gamejam/Characters/CharacterStat.cs
+++ compsoc-ascii-gamejam/Characters/CharacterStat.cs
@@ -1,8 +1,11 @@
-﻿namespace compsoc_ascii_gamejam.Characters;
+﻿using System.ComponentModel;
+
+namespace compsoc_ascii_gamejam.Characters;
 
 public enum CharacterStat
 {
     Health,
+    [Description("Max Health")]
     MaxHealth,
     Strength,
     Agility,
