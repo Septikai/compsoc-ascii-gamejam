e7d966e4fc1d045f9d897d31a115d6ab9e7d7a1e
Natalia Spence <52349324+Septikai@users.noreply.github.com>
2023-11-15 00:29:33 +0000

    correct previous commit with addition of an extension method

--- compsoc-ascii-gamejam/Characters/CharacterStat.cs
+++ compsoc-ascii-gamejam/Characters/CharacterStat.cs
@@ -5,9 +5,22 @@ namespace compsoc_ascii_gamejam.Characters;
 public enum CharacterStat
 {
     Health,
-    [Description("Max Health")]
     MaxHealth,
     Strength,
     Agility,
     Charisma
+}
+
+public static class CharacterStatExtensions
+{
+    public static String ToNiceString(this CharacterStat stat)
+    {
+        switch (stat)
+        {
+            case CharacterStat.MaxHealth:
+                return "Max Health";
+            default:
+                return stat.ToString();
+        }
+    }
 }
