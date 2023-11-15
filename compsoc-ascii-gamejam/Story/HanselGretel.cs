85d2dc0af35a01d945332bb8ec9e95f36d7560f7
Elliot Tollemache <114115893+ElLovesSWS@users.noreply.github.com>
2023-11-15 01:17:29 +0000

    HanselGretel complete

--- compsoc-ascii-gamejam/Story/HanselGretel.cs
+++ compsoc-ascii-gamejam/Story/HanselGretel.cs
@@ -4,6 +4,7 @@ using compsoc_ascii_gamejam.ConsoleIO;
 
 namespace compsoc_ascii_gamejam.Stories;
 
+//get this story by killing the wolf
 public class HanselGretel //start the story for hansel and gretel
 {
     private static readonly HanselGretel Instance = new HanselGretel();
@@ -11,7 +12,59 @@ public class HanselGretel //start the story for hansel and gretel
     private HanselGretel()
     {
         /*
-       
+        From a window in the cottage character will see hansel and gretel getting up
+        Confused character continues to walk to the cottage in order to make sure granny is alright and ask why there are children inside
+        Character walks in to find Hansel about to shove granny into the oven
+
+        RUN:
+        Character runs away to get help
+        Granny dies, character returns with help but the children are gone
+
+        TALK:
+        Kids explain what happened
+        Character gets choice on wether or not to believe them
+            Believe 
+            Character is mortified and lets kids continue in shock finishing the story and go into an outro of character being in shock about what happened and eventually goes home
+            crying to their mother
+
+            Don't Believe
+            Character will say to the kids there must have been a misunderstanding and tells them to leave granny alone
+            Charisma roll if char. wins kids will leave
+            Character will comfort granny and give her the foood and granny acts very thankful and says that she has no idea who those kids were acting all innocent
+            CHaracter goes home and good ending occurs
+
+        FIGHT:
+        Character immediately attacks kids, combat mechanic occurs, kids attack as one entity however each with low stats but added are quite strong
+        Character will kill the children, however in the process drop the basket causing the fruit to fall on the floor and get stepped on so granny now has no food to eat
+        in return granny will turn to the character
+
+         GRANNY IS NOW THE VILLAIN
+         RUN:
+         agility roll for character against granny
+         Character will run away from granny as fast as possible
+            Sucess:
+            Character successfully gets home and away from granny explains what happened and either transfer into other story or end with mum telling character not to make things up
+            and character gets sent to room ending game
+            Fail:
+            Granny catches up to character and then character loses 3 hp before escaping and continuing trying to make it home however granny will still be chasing character leading
+            to mum seeing and stepping in to save character
+
+        TALK:
+        charisma roll against granny
+        Character will attempt to convince granny to go home and this will all remain a secret and character will explain to mother that they tripped on the way and lost the goodies
+            Success:
+            Granny agrees and they walk home however this means an evil is still remaining so that is a loss of a WP
+            Fail:
+            Granny says no and kills character resulting in a game over
+
+        FIGHT:
+        fight mechanic
+        Character will launch into self defense mode
+            Success:
+            Character will kill granny getting a WP however will feel upset.
+            Character will feel too scared to go home therefore they will continue to walk through the woods triggering another story/an ending
+            Fail:
+            Character tries to defend from Granny however granny will throw into the oven and munch on
         */
     }
 
