57e4b02b667801a3acbf44dbf65335f212fb96a9
Elliot Tollemache <114115893+ElLovesSWS@users.noreply.github.com>
2023-11-15 02:02:58 +0000

    boycriedwolf plot added

--- compsoc-ascii-gamejam/Story/BoyWhoCriedWolf.cs
+++ compsoc-ascii-gamejam/Story/BoyWhoCriedWolf.cs
@@ -11,7 +11,39 @@ public class BoyCriedWolf //start the story for hansel and gretel
     private BoyCriedWolf()
     {
         /*
-       
+       You continue running eventually losing the wolf
+       You see the shephard boy at the top of the hill watching the sheep
+       You tell him about what just transpired and he begins to shout about the wolf
+       The towns people run up the hill ready to help protect the sheet only to be upset when there is no wolf
+       They yell at the two of you not to cause a panic when there is none and look at the shepard boy explaing
+       this is the second time he's done this and he needs to knock it off.
+       Eventually the wolf finds you and runs to you and the herd scaring the sheep
+       RUN:
+        agility roll against wolf
+        You try to run away again from the wolf
+            Sucess:
+            You run away, and eventually find your way home and explain to your mother what happened not in trouble and mother walks you to grannys where you have a simple ending
+            Fail:
+            The wolf catches you and attacks you (-6 hp) if you survive this then you simply run home and same as the sucess ending
+        TALK:
+        charisma roll against wolf
+        You beg the wolf to please just leave you be and apologize for offending it, offering it the fruits that were meant for granny
+            Sucess:
+            The wolf takes them and leaves you be. You go home and explain to your mother. She is upset and you both walk the bottle of wine back to grannys, where she then prepares dinner
+            for the 3 of you and you enjoy it with a peaceful ending
+            Fail:
+            The wolf does not take the grapes and instead chases off the sheep no loss to hp but the sheep are gone resulting in the boy getting in trouble and being upset with you 
+            for bringing the wolf towards the sheep 
+        FIGHT:
+        fighting mechanic 
+        Attack the wolf
+            Sucess:
+            You kill the wolf, bring the corpse to the villagers so they can use it and they are very happy as the wolf has been causing problems (it's the only wolf in the forest ig?)
+            In return you are given a cake which you take home to your mother. game over win game
+            Fail:
+            You attempt to fight the wolf however it attacks you as well (-6 hp) you begin crying out however causing the villagers to see what is the matter, they are upset and save you
+            however the sheep have gone, and the shephard boy is upset you brought the wolf over and someone helps you home to your mother where you immediately are made to rest to recover
+            game over wolf is still out there L
         */
     }
 
